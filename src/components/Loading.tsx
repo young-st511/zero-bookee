5ec4577e83eaa302a6aef8482cd83e8be8a95f0a
young-st511 <asdk511@gmail.com>
2022-11-11 13:08:52 +0900

    ✨ 로딩 스피너 추가

---
+++ src/components/Loading.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import spinner from "../assets/Spin-1s-200px.gif";
+
+export default function Loading({ text }: { text: string }) {
+  return (
+    <Background>
+      <LoadingText>{text}</LoadingText>
+      <img src={spinner} alt="로딩 중" width="5%" />
+    </Background>
+  );
+}
+
+const Background = styled.div`
+  position: absolute;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: #ffffffb7;
+  z-index: 999;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LoadingText = styled.p`
+  font: 1rem;
+  text-align: center;
+`;
