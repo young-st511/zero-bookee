6176aba836560cb193932f01ae6d5eff07a006f7
Jung YoungHun <56586322+young-st511@users.noreply.github.com>
2023-01-21 15:41:59 +0900

    🧑‍💻 ImpDev: Open Chrome at Vite server startup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+
+  if (isDev) {
+    process.env.BROWSER = "Google Chrome";
+  }
+
+  return {
+    plugins: [react()],
+    server: {
+      open: isDev,
+    },
+  };
+});
